bec4d7f3330dd27c6a1248c97606c3f0d2fbe9f4
Franz Luther Neulist Carroll <franz@franzneulistcarroll.com>
2021-12-19 09:28:33 +1100

    Initial commit with helper scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bandcamp Collection Downloader scripts
+
+Scripts to setup and run: https://framagit.org/Ezwen/bandcamp-collection-downloader
+
+## Requirements
+
+- `wget`
+- `jdk`
+
+## Usage
+
+Run `./scripts/init` and follow the directions in the output.
---
+++ config-template.sh
@@ -0,0 +1,8 @@
+#!/usr/local/env bash
+
+export DOWNLOAD_PATH="${HOME}/music/bandcamp-collection-downloader/"
+export AUDIO_FORMAT="flac"
+export BANDCAMP_USERNAME="YOUR BANDCAMP USERNAME"
+
+export COOKIES_PATH="${PWD}/cookies.json"
+export RELEASE_URL="https://framagit.org/Ezwen/bandcamp-collection-downloader/-/jobs/1515933/artifacts/raw/build/libs/bandcamp-collection-downloader.jar"
